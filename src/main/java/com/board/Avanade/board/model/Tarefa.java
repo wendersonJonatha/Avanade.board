5809e5318deaa6f5eeccd9fe6b9e34c595bc78d6
wendersonJ <wendersonjonathadev@gmail.com>
2025-03-20 21:06:23 -0300

    Criação da Tarefa Model

---
+++ src/main/java/com/board/Avanade/board/model/Tarefa.java
@@ -0,0 +1,84 @@
+package com.board.Avanade.board.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Tarefa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String descricao;
+    private String status;
+    private LocalDate dataCriacao;
+
+    // Construtor padrão (obrigatório para JPA)
+    public Tarefa() {}
+
+    // Construtor com parâmetros
+    public Tarefa(String titulo, String descricao, String status) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.status = status;
+        this.dataCriacao = LocalDate.now();
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", status='" + status + '\'' +
+                ", dataCriacao=" + dataCriacao +
+                '}';
+    }
+}
+
